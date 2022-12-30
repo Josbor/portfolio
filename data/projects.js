67d99fdfee32bf4323f028fd6de2c050a99c0e7d
Josbor <josbor.dev@gmail.com>
2022-12-30 11:24:00 -0400

    update

---
+++ data/projects.js
@@ -0,0 +1,9 @@
+export const projects=[
+    {   id:1,
+        name:'cotizador Cripto',
+        description:'este es un proyecto para consultar los datos de las diferentes cripto currencys',
+        projectUrl:'https://github.com/Josbor/cripto-react',
+        deploy:'https://cripto-consulta.netlify.app/',
+        image:''
+    }
+]
